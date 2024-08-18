41d04f5457253b48cb2239c18143407f11d8a61e
alvinfdl21 <alvinfdl21@gmail.com>
2024-08-18 09:31:17 +0700

    initial commit

---
+++ src/main/java/com/personal/freelance_manager/FreelanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.personal.freelance_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/freelance_manager/FreelanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.freelance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
