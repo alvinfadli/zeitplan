e98b9a6b9068239f9b1504df4d45cac338a06532
alvinfdl21 <alvinfdl21@gmail.com>
2024-08-20 20:00:24 +0700

    feat(test): auth

---
+++ src/test/java/com/personal/freelance_manager/controller/AuthControllerTest.java
@@ -0,0 +1,120 @@
+package com.personal.freelance_manager.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.freelance_manager.entity.User;
+import com.personal.freelance_manager.model.LoginUserRequest;
+import com.personal.freelance_manager.model.TokenResponse;
+import com.personal.freelance_manager.model.WebResponse;
+import com.personal.freelance_manager.repository.UserRepository;
+import com.personal.freelance_manager.security.BCrypt;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(){
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void loginFailed() throws Exception {
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("test");
+
+        mockMvc.perform(
+            post("/api/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+            status().isUnauthorized()
+        ).andDo(
+            result -> {
+                WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
+
+                assertNotNull(response.getErrors());
+            }
+        );
+    }
+
+    @Test
+    void loginFailedWrongPassword() throws Exception {
+        User user = new User();
+        user.setName("Test");
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("wrong");
+
+        mockMvc.perform(
+            post("/api/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+            status().isUnauthorized()
+        ).andDo(
+            result -> {
+                WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
+
+                assertNotNull(response.getErrors());
+            }
+        );
+    }
+
+    @Test
+    void loginSuccess() throws Exception {
+        User user = new User();
+        user.setName("Test");
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("test");
+
+        mockMvc.perform(
+            post("/api/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+            status().isOk()
+        ).andDo(
+            result -> {
+                WebResponse<TokenResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
+
+                assertNull(response.getErrors());
+                assertNotNull(response.getData().getExpiredAt());
+
+                User userDB = userRepository.findById("test").orElse(null);
+                assertNotNull(userDB);
+                assertEquals(userDB.getToken(), response.getData().getToken());
+                assertEquals(userDB.getTokenExpiredAt(), response.getData().getExpiredAt());
+            }
+        );
+    }
+}
